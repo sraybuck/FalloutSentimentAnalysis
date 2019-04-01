f5063e766ed4875ab86105f2441ac95be5f493a5
sraybuck <suzraybuck@gmail.com>
2019-03-31 21:01:56 -0400

    added stopwords, tokenized headlines

--- headlines.py
+++ headlines.py
@@ -4,44 +4,86 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pprint import pprint
 import nltk
+from nltk.corpus import stopwords
 
+#method definition for tokenization later
+def clean_tokens(a_list):
+    #tokenize and clean list
+    words = nltk.word_tokenize(a_list)
+    clean_words = []
+    for word in words:
+        clean_words.append(word.lower())
+    return clean_words
+
+#metadata so that google doesn't think I'm a robot
 headers = {
     "User-Agent":
         "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 }
+#the google query that im making
 payload = {'as_epq': 'Fallout 76', 'tbs':'cdr:1,cd_min:11/14/2018,cd_max:12/14/2018', 'tbm':'nws'}
+#actually going and talking to the website with the metadata and query
 r = requests.get("https://www.google.com/search", params=payload, headers=headers)
 
+#print url to verify its correct
 print(r.url)
 
+#create BeautifulSoup object out of the text from the requested site
 soup = bs4.BeautifulSoup(r.text, 'lxml')
 
+#select only the headlines in each google search result
 base = soup.select("div.g.card h3")
 
+#declare empty list where I'm going to put all the headlines
 headlines = []
 
+#loop to get rid of all html and keep only headline text
 for row in base:
     clean = row.text
     headlines.append(clean)
 
+#print to verify headlines are clean
 print(headlines)
 
-import nltk
+#empty list to store tokenized headlines
+tokens = []
+
+#loop to tokenize headlines by using clean_tokens method from earlier
+for count, each in enumerate(headlines):
+    clean = clean_tokens(headlines[count])
+    tokens.append(clean)
+
+#print to verify
+print(tokens)
+
+#create stopwords list from nltk and add fallout 76 as stopwords
+stopwords = nltk.corpus.stopwords.words('english')
+stopwords.append("fallout")
+stopwords.append('76')
+stopwords.append("'fallout")
+
+print(stopwords)
+
+filtered_headline = []
+filtered_headlines = []
+
+for listitem in tokens:
+
+
+print(*filtered_headline, sep = "\n")
+
+""""
 nltk.download('vader_lexicon')
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 
 sia = SIA()
 results = []
-count = 0
 
-for line in headlines:
+for count,line in enumerate(headlines):
     pol_score = sia.polarity_scores(line)
     pol_score['headline'] = headlines[count]
-    count = count + 1
     results.append(pol_score)
 
-pprint(results)
-
-df = pd.DataFrame.from_records(results)
-df.head()
+print(*results, sep = "\n")
+"""
